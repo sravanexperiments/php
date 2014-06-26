6e2da10eaf468b73c309b6d78236a84da54651cb
pavan chand <pavan chand@pavanchand.mshome.net>
2014-06-27 01:02:04 +0530

    start

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic='E:\instalations\wamp\www\restapi\framework\yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
